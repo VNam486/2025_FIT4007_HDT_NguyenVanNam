0e5811c9d212c4d0a1cb223f9b62af1432034c83
Nam-code <nguynzannem@gmail.com>
2025-09-19 19:33:00 +0700

    update all files

--- Circle.java
+++ Circle.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
 public class Circle {
-    // Thuộc tính
     private double r;
 
-    // Constructor không tham số (default) -> r = 1
     public Circle() {
         this.r = 1;
     }
 
-    // Constructor có tham số
+    
     public Circle(double r) {
         this.r = r;
     }
 
-    // Getter & Setter
     public double getR() {
         return r;
     }
@@ -23,47 +20,39 @@ public class Circle {
         this.r = r;
     }
 
-    // Nhập bán kính từ bàn phím
-    public void input() {
-        Scanner sc = new Scanner(System.in);
+    
+    public void input(Scanner sc) {
         System.out.print("Nhap ban kinh r: ");
         this.r = sc.nextDouble();
     }
 
-    // Xuất thông tin
     public void output() {
         System.out.println("Thong tin hinh tron:");
         System.out.println("Ban kinh r: " + r);
     }
 
-    // Tính diện tích
     public double tinhDienTich() {
         return Math.PI * r * r;
     }
 
-    // Tính chu vi
     public double tinhChuVi() {
         return 2 * Math.PI * r;
     }
 
-    // Main
     public static void main(String[] args) {
-        // Tạo hình tròn mặc định
-        Circle c1 = new Circle();
+        Scanner sc = new Scanner(System.in); 
 
-        // Xuất thông tin mặc định
+        Circle c1 = new Circle(); 
         c1.output();
 
-        // Nhập bán kính mới
         System.out.println("\nNhap hinh tron:");
-        c1.input();
+        c1.input(sc);
 
-        // Xuất thông tin sau khi nhập
         c1.output();
+        System.out.printf("Dien tich hinh tron: %.2f\n", c1.tinhDienTich());
+        System.out.printf("Chu vi hinh tron: %.2f\n", c1.tinhChuVi());
 
-        // In diện tích và chu vi
-        System.out.println("Dien tich hinh tron: " + c1.tinhDienTich());
-        System.out.println("Chu vi hinh tron: " + c1.tinhChuVi());
+        sc.close(); 
     }
 }
 
